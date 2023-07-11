cd13776fc941cfb548775991064c8e07459d88b7
Merve UĞURSAÇ <merve.ugursac34@gmail.com>
2023-07-11 21:03:20 +0300

    Add project files.

---
+++ ImageAddWatermarkApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageAddWatermarkApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
